90e1df43f62b9df9b37612bc7d424be6c7cc5af2
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-07-03 18:39:41 +0700

    :construction: (ui) add navbar

--- app/ui/pages/_app.js
+++ app/ui/pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 
 import { Provider } from "mobx-react";
+import NavBar from "../components/NavBar";
 import CartStore from "../stores/CartStore";
 import ProductStore from "../stores/ProductStore";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider cartStore={CartStore} productStore={ProductStore}>
+      <NavBar />
       <Component {...pageProps} />
     </Provider>
   );
