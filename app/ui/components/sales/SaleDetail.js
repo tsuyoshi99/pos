f66724b0a4923401b0b915dfd8d72a48af89ff52
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-07-30 14:30:00 +0700

    :page_facing_up: (sale) create sale detail modal display

---
+++ app/ui/components/sales/SaleDetail.js
@@ -0,0 +1,13 @@
+export default function SaleDetail() {
+  return (
+    <label htmlFor="sale-detail-modal" className="modal cursor-pointer">
+      <label className="modal-box relative" htmlFor="">
+        <h3 className="text-lg font-bold">Reciept Detail</h3>
+        <p className="py-4">
+          You have been selected for a chance to get one year of subscription to
+          use Wikipedia for free!
+        </p>
+      </label>
+    </label>
+  );
+}
