89860473792280e7986a313ae8ae60a8af72aecc
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-07-30 14:30:00 +0700

    :page_facing_up: (sale) create sale detail modal display

--- app/ui/components/sales/SaleCard.js
+++ app/ui/components/sales/SaleCard.js
@@ -1,23 +1,18 @@
 export default function SaleCard(props) {
   const sale = props.saleHistory;
-  const date = convertDate(sale.createdAt);
+  const date = Date.parse(sale.createdAt);
 
-  function convertDate(date) {
-    const formatYmd = (date) => date.toISOString().slice(0, 10);
-
-    // Example
-    formatYmd(new Date());
-
-    // formatYmd.split("-").reverse().join("-");
-    return formatYmd;
-  }
   return (
     <div className="card bg-base-100 shadow-xl border">
       <div className="card-body">
         <h2 className="card-title">{props.saleHistory.user.email}</h2>
         <div className="flex justify-between">
-          <p>Total cost: {sale.total}</p>
-          <p>Purchased on: {date}</p>
+          <p>Total cost: </p>
+          <p className="text-end">${sale.total}</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Purchased on: </p>
+          <p className="text-end">{date}</p>
         </div>
       </div>
     </div>
--- app/ui/pages/sales.js
+++ app/ui/pages/sales.js
@@ -3,7 +3,8 @@ import Head from "next/head";
 import { inject, observer } from "mobx-react";
 
 import NavBar from "../components/NavBar";
-import SaleCard from "../components/SaleCard";
+import SaleCard from "../components/sales/SaleCard";
+import SaleDetail from "../components/sales/SaleDetail";
 
 function SalesHistory(props) {
   const { sales, getAllSales } = props.salesStore;
@@ -29,14 +30,22 @@ function SalesHistory(props) {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {sales.map((sale) => {
               return (
-                <div className="cursor-pointer" key={sale.id}>
+                <label
+                  key={sale.id}
+                  htmlFor="sale-detail-modal"
+                  className="cursor-pointer"
+                >
                   <SaleCard saleHistory={sale} />
-                </div>
+                </label>
               );
             })}
           </div>
         </main>
       </div>
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="sale-detail-modal" className="modal-toggle" />
+      <SaleDetail />
     </React.Fragment>
   );
 }
